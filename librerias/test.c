32f4452bc9efe4ba62bc5c3fde261ba3bcc9b4eb
Aldo Davila <aldoda2003gp5@gmail.com>
2025-02-24 19:41:07 -0600

    Limpiar Repo y actualizar .gitignore

--- librerias/test.c
+++
@@ -1,8 +0,0 @@
-#define EDLIB_MAIN
-#include "edlib.h"
-
-int main(void) {
-    const char * s1 = "Hello";
-    const char * s2 = "World";
-    println(s1, s2, "Pija", "Cocos", "Bulldog Frances");
-}
