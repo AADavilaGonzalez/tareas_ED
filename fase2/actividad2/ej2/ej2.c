148cd0a376370c5dc3ceee8771a9f387962fe27f
Roberto Sánchez Santoyo <robertosanchezsantoyo@outlook.com>
2025-03-21 20:13:16 -0600

    Ejercicio 2

---
+++ fase2/actividad2/ej2/ej2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void imprimir_lista(int min, int max){
+    if(min > max) return;
+    printf("%d ", min);
+    imprimir_lista(min+1, max);
+}
+
+int main(void){
+    int min, max, num;
+    printf("Introduzca el valor menor\n");
+    scanf("%d", &min),
+    printf("Introduzca el valor mayor\n");
+    scanf("%d", &max);
+
+    if(min < max)
+        imprimir_lista(min, max);
+    else;
+        imprimir_lista(max, min);
+    return 0;
+}
