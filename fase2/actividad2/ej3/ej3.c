617dc9891b12fbf2d5180ef9f84e1be4caa8293d
Airos1214 <air.14rdz@gmail.com>
2025-03-22 21:27:43 -0600

    Update ej3.c

--- fase2/actividad2/ej3/ej3.c
+++ fase2/actividad2/ej3/ej3.c
@@ -2,31 +2,29 @@
 #define EDLIB_MAIN
 #include "edlib.h"
 
-int invertir(int num)
+int invertir(int num, int aux)
 {
-    if(num < 10) //si el numero tiene nada mas un digito, se regresa el mismo numero
-    {
-        printf("%d", num);
-        return;
-    }
-    
-    printf("%d", num % 10); //se extrae el ultimo digito del numero y se imprime
-    
-    //se vuelve a llamar a la funcion pasandole como parametro el numero
-    //pero sin el ultimo digito
-    return invertir(num/10);
+    if(num == 0)
+        return aux; //se retorna la cantidad acumulada
+
+    return invertir(num / 10, aux * 10 + (num % 10)); //se quita el ultimo digito del numero y
+    //el ultimo digito se agrega al acumulador
+
 }
 
 int main()
 {
-    int num;
+    int num, resultado;
 
     //Se ingresa el numero que se desea invertir
     printf("Ingrese el numero a invertir:\n");
     num = validar_int_min(0);
-    
+
+
     printf("El numero inverso es: \n");
-    invertir(num); //se manda a llamar a la funcion para invertir el numero
+    //se manda a llamar a la funcion para invertir el numero
+    resultado = invertir(num, 0); //se agrega un acumulador para construir el inverso
+    printf("%d", resultado);
 
     return 0;
 }
