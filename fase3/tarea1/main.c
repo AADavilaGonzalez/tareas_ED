40402fd4f2a0621a6fffc7f5d912440b1592d2f9
Roberto Sánchez Santoyo <robertosanchezsantoyo@outlook.com>
2025-04-10 20:28:14 -0600

    Tarea-1 crear y insertar nodos

--- fase3/tarea1/main.c
+++ fase3/tarea1/main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void limpiar_pantalla();
-void crear_arbol();
-void buscar_elemento();
-void insertar_elemento();
-void eliminar_elemento();
-
 typedef struct nodo{
     int dato;
     struct nodo *izq;
     struct nodo *der;
 } Nodo;
 
+void limpiar_pantalla();
+Nodo* crear_nodo(int valor);
+Nodo* insertar_elemento(Nodo* raiz, int valor);
+void inorder(Nodo *raiz);
+Nodo* buscar_elemento(Nodo *raiz, int valor);
+void imprimir_arbol(Nodo* raiz, int nivel);
+
 int main(void){
-    int op1;
+    int op1, val = 0, num, i, valor;
+    Nodo *raiz = NULL;
 
     while(1){
         // Ciclo para validar el menu
@@ -24,8 +26,9 @@ int main(void){
                 "2.- Buscar un elemento del arbol\n"
                 "3.- Insertar un elemento en el arbol\n"
                 "4.- Eliminar un elemento del arbol\n"
-                "5.- Salir");
-            if(scanf("%d", &op1) != 1 || op1 < 1 || op1 > 5){
+                "5.- Imprimir el arbol\n"
+                "6.- Salir\n");
+            if(scanf("%d", &op1) != 1 || op1 < 1 || op1 > 6){
                 printf("Introduzca un número valido\n\n");
                 while(getchar() != '\n');
                 continue;
@@ -34,33 +37,66 @@ int main(void){
             break;
         }
         switch (op1){
-        case 1:
-            Nodo *raiz = NULL;
-            break;
-        case 2:
-            if(raiz != NULL){
-                buscar_elemento();
+            case 1:
+                if(val == 0){
+                    while(1){
+                        printf("Introduzca el numero de elementos a insertar: \n");
+                        if(scanf("%d", &num) != 1 || num < 0){
+                            printf("Introdzuca un número valido\n");
+                            while(getchar() != '\n');
+                            continue;
+                        }
+                        break;
+                    }
+                    for(i = 0; i < num; i++){
+                        printf("Introduzca un entero\n");
+                        if(scanf("%d", &valor) != 1){
+                            printf("Introdzuca un número valido\n");
+                            while(getchar() != '\n');
+                            continue;
+                        }
+                        raiz = insertar_elemento(raiz, valor);
+                    }
+                    printf("\nArbol: \n");
+                    imprimir_arbol(raiz, 0);
+                    printf("\n\n");
+                    val = 1;
+                    break;
+                }
+                if(val == 1) printf("El arbol ya ha sido creado\n\n");
                 break;
-            }
-            if(raiz == NULL){
-                printf("El arbol esta vacio\n");
-            }else{
-                printf("El arbol no esta creado\n");
-            }
-            break;
-        case 3:
-            insertar_elemento();
-            break;
-        case 4:
-            if(raiz == NULL){
-                printf("El arbol esta vacio\n");
-                continue;
-            }else{
-                eliminar_elemento();
-            }
-            break;
+            case 2:
+                break;
+            case 3:
+                if(val == 1){
+                    printf("Introduzca un entero\n");
+                    if(scanf("%d", &valor) != 1){
+                        printf("Introduzca un número valido\n");
+                        while(getchar() != '\n');
+                        continue;
+                    }
+                    insertar_elemento(raiz, valor);
+                    printf("Arbol: \n");
+                    imprimir_arbol(raiz, 0);
+                    
+                    printf("\n\n");
+                    break;
+                }
+                if(val == 0) printf("El arbol no a sido creado\n\n");
+                break;
+            case 4:
+                break;
+            case 5:
+                if(val == 1 && raiz != NULL){
+                    imprimir_arbol(raiz, 0);
+                    printf("\n\n");
+                    break;
+                }
+                if(val == 0) printf("El arbol no a sido creado\n\n");
+                if(raiz == NULL) printf("El arbol esta vacio\n\n");
+                break;
         }
-        if(op1 == 5){
+        if(op1 == 6){
             break;
         }
     }
@@ -75,17 +111,43 @@ void limpiar_pantalla(){
     #endif
 }
 
-void crear_arbol(){
-
+Nodo *crear_nodo(int valor){
+    Nodo *nuevo = (Nodo *)malloc(sizeof(Nodo));
+    if(nuevo == NULL){
+        printf("Error al asignar la memoria\n");
+        exit(1);
+    }
+    nuevo->dato = valor;
+    nuevo->izq = nuevo->der = NULL;
+    return nuevo;
 }
 
-void buscar_elemento(){
+Nodo *insertar_elemento(Nodo *raiz, int valor){
+    // Si el arbol no tiene elementos
+    if(raiz == NULL) return crear_nodo(valor);
+    // Si el valor es menor que la raiz, se coloca en la izquierda
+    if(valor < raiz->dato) raiz->izq = insertar_elemento(raiz->izq, valor);
+    // Si el dato es mayor que la raiz, se coloca a la derecha
+    else if(valor > raiz->dato) raiz->der = insertar_elemento(raiz->der, valor);
+    // Si es igual, no se inserta (Evita duplicados)
+    return raiz;
 }
 
-void insertar_elemento(){
-
+void inorder(Nodo *raiz){
+    if(raiz != NULL){
+        inorder(raiz->izq);
+        printf("%d ", raiz->dato);
+        inorder(raiz->der);
+    }
 }
 
-void eliminar_elemento(){
-
+void imprimir_arbol(Nodo* raiz, int nivel){
+    if (raiz == NULL) return;
+    // Rama derecha
+    imprimir_arbol(raiz->der, nivel + 1);
+    for (int i = 0; i < nivel; i++) printf("   ");
+    printf("%d\n", raiz->dato);
+    // Rama izquierda
+    imprimir_arbol(raiz->izq, nivel + 1);
 }
+
