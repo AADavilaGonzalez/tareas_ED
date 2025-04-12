23088e22a5fab3e269311097a36a3329f2dec4d4
Airos1214 <air.14rdz@gmail.com>
2025-04-12 14:40:40 -0600

    Add files via upload

---
+++ fase3/tarea1/main2.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define EDLIB_MAIN
+#include "edlib.h"
+typedef struct nodo{
+    int dato;
+    struct nodo *izq;
+    struct nodo *der;
+} Nodo;
+
+void limpiar_pantalla();
+Nodo* crear_nodo(int valor);
+Nodo* insertar_elemento(Nodo* raiz, int valor);
+Nodo* buscar_elemento(Nodo *raiz, int valor);
+void imprimir_arbol(Nodo* raiz, int nivel);
+
+int main(void){
+    int op1, val = 0, num, i, valor, elemento;
+    Nodo *raiz = NULL;
+
+    while(1){
+        // Ciclo para validar el menu
+        while(1){
+            printf("Seleccione una opcion.\n"
+                "1.- Crear un arbol binario\n"
+                "2.- Buscar un elemento del arbol\n"
+                "3.- Insertar un elemento en el arbol\n"
+                "4.- Eliminar un elemento del arbol\n"
+                "5.- Imprimir el arbol\n"
+                "6.- Salir\n");
+            if(scanf("%d", &op1) != 1 || op1 < 1 || op1 > 6){
+                printf("Introduzca un número valido\n\n");
+                while(getchar() != '\n');
+                continue;
+            }
+            limpiar_pantalla();
+            break;
+        }
+        switch (op1){
+            case 1:
+                if(val == 0){
+                    while(1){
+                        printf("Introduzca el numero de elementos a insertar: \n");
+                        if(scanf("%d", &num) != 1 || num < 0){
+                            printf("Introdzuca un número valido\n");
+                            while(getchar() != '\n');
+                            continue;
+                        }
+                        break;
+                    }
+                    for(i = 0; i < num; i++){
+                        printf("Introduzca un entero\n");
+                        if(scanf("%d", &valor) != 1){
+                            printf("Introdzuca un número valido\n");
+                            while(getchar() != '\n');
+                            continue;
+                        }
+                        raiz = insertar_elemento(raiz, valor);
+                    }
+                    printf("\nArbol: \n");
+                    imprimir_arbol(raiz, 0);
+                    printf("\n\n");
+                    val = 1;
+                    break;
+                }
+                if(val == 1) printf("El arbol ya ha sido creado\n\n");
+                break;
+            case 2:
+                if(val == 1){
+                    while(1)
+                    {
+                        printf("Introduzca el elemento a buscar: (-1 para salir)\n");
+                        valor = validar_int_min(-1);
+
+                        if(valor == -1)
+                            break;
+                
+                        elemento = buscar_elemento(raiz, valor);
+                        if(elemento != NULL)
+                        {
+                            printf("Elemento encontrado: %d\n", elemento);
+                            printf("\nArbol: \n");
+                            imprimir_arbol(raiz, 0);
+                            printf("\n\n");
+                        }
+                        else
+                            printf("Elemento no encontrado\n");
+                    }
+                }
+                if(val == 0) printf("El arbol no a sido creado\n\n");
+                else if(raiz == NULL) printf("El arbol esta vacio\n\n");
+                break;
+            case 3:
+                if(val == 1){
+                    printf("Introduzca un entero\n");
+                    if(scanf("%d", &valor) != 1){
+                        printf("Introduzca un número valido\n");
+                        while(getchar() != '\n');
+                        continue;
+                    }
+                    insertar_elemento(raiz, valor);
+                    printf("Arbol: \n");
+                    imprimir_arbol(raiz, 0);
+
+                    printf("\n\n");
+                    break;
+                }
+                if(val == 0) printf("El arbol no a sido creado\n\n");
+                break;
+            case 4:
+                break;
+            case 5:
+                if(val == 1 && raiz != NULL){
+                    imprimir_arbol(raiz, 0);
+                    printf("\n\n");
+                    break;
+                }
+                if(val == 0) printf("El arbol no a sido creado\n\n");
+                if(raiz == NULL) printf("El arbol esta vacio\n\n");
+                break;
+        }
+        if(op1 == 6){
+            break;
+        }
+    }
+    return 0;
+}
+
+void limpiar_pantalla(){
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+Nodo *crear_nodo(int valor){
+    Nodo *nuevo = (Nodo *)malloc(sizeof(Nodo));
+    if(nuevo == NULL){
+        printf("Error al asignar la memoria\n");
+        exit(1);
+    }
+    nuevo->dato = valor;
+    nuevo->izq = nuevo->der = NULL;
+    return nuevo;
+}
+
+Nodo *insertar_elemento(Nodo *raiz, int valor){
+    // Si el arbol no tiene elementos
+    if(raiz == NULL) return crear_nodo(valor);
+    // Si el valor es menor que la raiz, se coloca en la izquierda
+    if(valor < raiz->dato) raiz->izq = insertar_elemento(raiz->izq, valor);
+    // Si el dato es mayor que la raiz, se coloca a la derecha
+    else if(valor > raiz->dato) raiz->der = insertar_elemento(raiz->der, valor);
+    // Si es igual, no se inserta (Evita duplicados)
+    return raiz;
+}
+
+void imprimir_arbol(Nodo* raiz, int nivel){
+    if (raiz == NULL) return;
+    // Rama derecha
+    imprimir_arbol(raiz->der, nivel + 1);
+    for (int i = 0; i < nivel; i++) printf("   ");
+    printf("%d\n", raiz->dato);
+    // Rama izquierda
+    imprimir_arbol(raiz->izq, nivel + 1);
+}
+
+Nodo* buscar_elemento(Nodo *raiz, int valor)
+{
+    if(raiz == NULL)//regresa null por si no encontro el elemento
+        return NULL;
+        
+    if(raiz->dato == valor) // si el valor pedido es el mismo que el dato en el nodo se regresa el valor
+        return raiz -> dato;
+
+    if(valor < raiz -> dato)//si el valor es menor que el dato del nodo se va a para la izquierda
+        return buscar_elemento(raiz -> izq, valor);
+    else
+        return buscar_elemento(raiz-> der, valor);//por el contrario se va a la derecha si es mayor
+    
+}
